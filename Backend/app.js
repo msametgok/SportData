851475fb208cf242c59268da8f874c35cb88cf5f
Mehmet Samet Gok <msametgok@hotmail.com>
2023-02-20 14:50:46 -0500

    user route and parsing app settings added

--- Backend/app.js
+++ Backend/app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const mongoose = require("mongoose");
 const pageRoute = require('./routes/pageRoute')
+const userRoute = require('./routes/userRoute')
+
+
+//mongodb+srv://msametgok:<password>@sportdatadb.pllmv0s.mongodb.net/?retryWrites=true&w=majority
+//mongodb://127.0.0.1/sportdata
 
 mongoose.set("strictQuery", false);
 //Connect DB
-mongoose.connect("mongodb://127.0.0.1/sportdata").then(() => {
+mongoose.connect("mongodb+srv://msametgok:eAjn5pwVTtEPii9t@sportdatadb.pllmv0s.mongodb.net/?retryWrites=true&w=majority").then(() => {
   console.log("Connection successful");
 }).catch(err => {
     console.log(err);
@@ -13,10 +18,12 @@ mongoose.connect("mongodb://127.0.0.1/sportdata").then(() => {
 const app = express();
 
 //Middlewares
-
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 //Routes
 app.use('/',pageRoute);
+app.use('/users', userRoute)
 
 const port = 3000;
 
