6c980a8a695c234cc3695c549a20e984a2b5694c
Mehmet Samet Gok <msametgok@hotmail.com>
2023-02-21 15:14:36 -0500

    user login done

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,6 +1,6 @@
 const User = require('../models/User')
 
-exports.createUser = async (req, res) => {
+exports.createUser = async (req, res) => { 
     try {
         const user = await User.create(req.body);
         console.log(user);
@@ -11,6 +11,31 @@ exports.createUser = async (req, res) => {
     } catch (error) {
         res.status(400).json({
             status: 'fail',
+            error
+        })
+    }
+}
+
+exports.loginUser = async (req, res) => {
+    try {
+        const {email, password} = req.body;
+        await User.findOne({email}).then(user => {
+            if(password === user.password) {
+                res.status(200).json({
+                    status: 'success',
+                    user
+                })
+            } else {
+                res.status(400).json({
+                    status: 'fail',
+                    msg: 'Wrong password'
+                })
+            }
+        })
+    } catch (error) {
+        res.status(400).json({
+            status: 'fail',
+            msg: 'No users found',
             error
         })
     }
