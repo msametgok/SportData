e66ee7f1ddf9ce543c2f2c2414243f4ef7849c73
Mehmet Samet Gok <msametgok@hotmail.com>
2023-02-22 16:40:53 -0500

    User login comparing password with encrypted password

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,42 +1,44 @@
-const User = require('../models/User')
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
-exports.createUser = async (req, res) => { 
-    try {
-        const user = await User.create(req.body);
-        console.log(user);
-        res.status(201).json({
-            status: 'success',
-            user
-        });
-    } catch (error) {
-        res.status(400).json({
-            status: 'fail',
-            error
-        })
-    }
-}
+exports.createUser = async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.status(201).json({
+      status: "success",
+      user,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      error,
+    });
+  }
+};
 
 exports.loginUser = async (req, res) => {
-    try {
-        const {email, password} = req.body;
-        await User.findOne({email}).then(user => {
-            if(password === user.password) {
-                res.status(200).json({
-                    status: 'success',
-                    user
-                })
-            } else {
-                res.status(400).json({
-                    status: 'fail',
-                    msg: 'Wrong password'
-                })
-            }
-        })
-    } catch (error) {
-        res.status(400).json({
-            status: 'fail',
-            msg: 'No users found',
-            error
-        })
-    }
-}
+  try {
+    const { email, password } = req.body;
+    await User.findOne({ email }).then((user) => {
+      bcrypt.compare(password, user.password, (err, same) => {
+        if (same) {
+          res.status(200).json({
+            status: "success",
+            user,
+          });
+        } else {
+          res.status(400).json({
+            status: "fail",
+            msg: "Wrong password",
+          });
+        }
+      });
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      msg: "No users found",
+      error,
+    });
+  }
+};
